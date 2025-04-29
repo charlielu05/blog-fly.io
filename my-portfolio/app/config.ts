5bcec3e7ad549c9708e9343a20ea1170d552899d
charlie <charlielu05@gmail.com>
2025-04-29 04:26:31 +0000

    init comit

---
+++ my-portfolio/app/config.ts
@@ -0,0 +1,14 @@
+export const metaData = {
+  baseUrl: "https://nextfolio-template.vercel.app/",
+  title: "Charlie Lu",
+  name: "Charlie Lu",
+  ogImage: "/opengraph-image.png",
+  description:
+    "A personal blog/portfolio. I write about stuff that are intersting to me.",
+};
+
+export const socialLinks = {
+  github: "https://github.com/charlielu05",
+  linkedin: "www.linkedin.com/in/charlie-d-lu",
+  email: "mailto:charlielu05@gmail.com",
+};
